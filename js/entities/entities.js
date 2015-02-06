ab7c0a18816d93537305b7e439b12cf2de41b779
David Hearn <davidhearn90@gmail.com>
2015-02-05 20:20:15 -0800

    finished video 14

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -38,9 +38,22 @@ game.PlayerEntity = me.Entity.extend({
 
 		}
 
+		else if (me.input.isKeyPressed("left")) {
+
+			this.body.vel.x -=this.body.accel.x * me.timer.tick;
+			this.flipX(false);
+		}
+
 		else{
 			this.body.vel.x = 0;
 		}
+
+		if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling) {
+
+				this.jumping = true;
+				this.body.vel.y -=this.body.accel.y * me.timer.tick;
+
+		};
 
 	 if (this.body.vel.x !== 0) {
 		if (!this.renderable.isCurrentAnimation("walk")) {
@@ -94,7 +107,7 @@ game.PlayerBaseEntity = me.Entity.extend({
 			spriteheight:"100",
 			getShape: function(){
 
-				return(new me.Rect(0, 0, 100, 100)).toPolygon();
+				return(new me.Rect(0, 0, 100, 70)).toPolygon();
 			}
 		}])
 
@@ -146,7 +159,7 @@ game.EnemyBaseEntity = me.Entity.extend({
 			spriteheight:"100",
 			getShape: function(){
 
-				return(new me.Rect(0, 0, 100, 100)).toPolygon();
+				return(new me.Rect(0, 0, 100, 70)).toPolygon();
 			}
 		}])
 
