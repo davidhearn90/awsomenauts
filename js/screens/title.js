a2446deae6c72c1482254cc4161f81ed4ee0f8e4
David Hearn <davidhearn90@gmail.com>
2015-03-06 18:58:10 -0800

    finished video 40

--- js/screens/title.js
+++ js/screens/title.js
@@ -32,6 +32,29 @@ game.TitleScreen = me.ScreenObject.extend({
                     }
                     
                 })));
+
+                 me.game.world.addChild(new(me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, "init",[380, 340, 250, 50]);
+                        this.font = new me.Font("Arial", 46, "white");
+                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+                    },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+//                        this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
+                    },
+                    
+                    update: function(dt){
+                      return true;  
+                    },
+                    newGame: function(){
+                       me.input.releasePointerEvent('pointerdown', this);
+                       me.state.change(me.state.PLAY);
+                       
+                    }
+                    
+                })));
 //                this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keycode, edge){
 //                   if(action === "start") {
 //                       me.state.change(me.state.PLAY);
