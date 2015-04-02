48aaaab71185a8e006698427f0d7ce7006097b70
David Hearn <davidhearn90@gmail.com>
2015-04-02 08:30:27 -0700

    fixed video 51

---
+++ js/gamemanagers/GameManager.js
@@ -0,0 +1,32 @@
+
+
+
+
+game.ExperienceManager = Object.extend({
+    init: function(x, y, setttings){
+        this.alwaysUpdate = true;
+        this.gameover = false;
+    },
+    update: function(){
+        if(game.data.win === true && this.gameover){
+            this.gameOver(true);
+            //game.data.gameOver = true;
+        }else if(game.data.win === false && !this.gameover){
+           this.gameOver(false);
+        }
+        
+        return true;
+    },
+    gameOver: function(win){
+        if(win){
+           game.data.exp += 10;
+        }else{
+           game.data.exp += 1;  
+        }
+            this.gameover = true;
+            me.save.exp = game.data.exp;
+            //For testing purposes only
+            // me.save.exp2 = 4;
+    }
+});
+
